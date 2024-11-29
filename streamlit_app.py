daea94606160c8f3bf8020b8f0554c51f1b03b3e
andre123354cs <edwar.vaco@gmail.com>
2024-11-29 11:19:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,8 +140,9 @@ with tab3:
         conn.close()
     
         # Obtener los nombres de los funcionarios únicos
-        funcionarios = Global['fecha'].unique()
-    
+        #funcionarios = Global['fecha'].unique()
+        funcionarios = ['Todos'] + list(Global['fecha'].unique())
+
         # Crear un selectbox para elegir el funcionario
         funcionario_seleccionado = st.multiselect("Seleccionar funcionario", funcionarios)
     
