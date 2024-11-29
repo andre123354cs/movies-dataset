0b1d85c587bb62dc7bcf3d31a67a6e0ad6e5e317
andre123354cs <edwar.vaco@gmail.com>
2024-11-29 10:45:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,7 +152,6 @@ with tab3:
           title='Número de novedades por funcionario'
         )
         st.altair_chart(chart, use_container_width=True)
-    filtrar_y_visualizar()
     
 
 # Ejecutar la aplicación
