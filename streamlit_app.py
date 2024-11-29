080046b2fc2b8ee0b47f05b7744d8c578ed455bf
andre123354cs <edwar.vaco@gmail.com>
2024-11-29 11:01:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,28 +131,7 @@ with tab3:
         conn.close()
 
         st.dataframe(df)
-        # Filtrar por fechas
-        df_filtrado = df[(df['fecha'] >= fecha_inicio) & (df['fecha'] <= fecha_fin)]
-        
-        # Mostrar tabla con los resultados
-        st.dataframe(df_filtrado)
-        
-        # Contar las novedades por funcionario y mostrar en una tabla
-        conteo_novedades = df_filtrado.groupby('nombre_funcionario').size().reset_index(name='Total_Novedades')
-        st.dataframe(conteo_novedades)
-        
-        # Crear gráfico de barras
-        chart = alt.Chart(df_filtrado).mark_bar().encode(
-          x='nombre_funcionario',
-          y='count()',
-          tooltip=['novedad']
-        ).properties(
-          title='Número de novedades por funcionario'
-        )
-        st.altair_chart(chart, use_container_width=True)
-
-    filtrar_y_visualizar()
- 
+     
     
 # Ejecutar la aplicación
 if __name__ == "__main__":
