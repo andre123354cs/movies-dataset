f1596eb48cf92a90c96d5d760f035caf799aa94f
andre123354cs <edwar.vaco@gmail.com>
2024-11-29 09:10:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,6 @@ st.set_page_config(page_title="Movies dataset", page_icon="🌱")
 st.title("🌱 RRHH YesBpo")
 st.write("Transparencia y claridad en cada paso. Conoce el estado de tus solicitudes y mantente informado sobre los procesos de RRHH. ¡Tu tranquilidad es nuestra prioridad!")
 
-@st.cache_data
-def load_data():
-    df = pd.read_csv("data/Libro1.csv")
-    return df
-df = load_data()
-
-st.dataframe(df)
-
-import streamlit as st
-import pandas as pd
 
 @st.cache_data
 def load_data():
