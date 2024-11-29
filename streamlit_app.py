ab6fd6ed239324cd297e9fd7ae80962748ad437e
andre123354cs <edwar.vaco@gmail.com>
2024-11-29 09:06:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,10 @@ st.set_page_config(page_title="Movies dataset", page_icon="🌱")
 st.title("🌱 RRHH YesBpo")
 st.write("Transparencia y claridad en cada paso. Conoce el estado de tus solicitudes y mantente informado sobre los procesos de RRHH. ¡Tu tranquilidad es nuestra prioridad!")
 
-
-# Load the data from a CSV. We're caching this so it doesn't reload every time the app
-# reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
     df = pd.read_csv("data/Libro1.csv")
     return df
-
-
 df = load_data()
 
 st.dataframe(df)
