ddab0cfa10f8b2d9d2bce978e0d9aab3ff7b89ed
andre123354cs <edwar.vaco@gmail.com>
2024-11-29 11:16:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,7 +140,7 @@ with tab3:
         conn.close()
     
         # Obtener los nombres de los funcionarios únicos
-        funcionarios = df['fecha'].unique()
+        funcionarios = df['nombre_funcionario'].unique()
     
         # Crear un selectbox para elegir el funcionario
         funcionario_seleccionado = st.selectbox("Seleccionar funcionario", funcionarios)
